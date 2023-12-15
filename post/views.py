3efff90820662837916a960958926171c701bc91
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2023-12-15 20:22:23 +0500

    added posts list

--- post/views.py
+++ post/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from post.serializers import PostSerializer
+
 
 # Create your views here.
+
+
+class PostListView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
